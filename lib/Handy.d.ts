4bb8a8cf2b9c692d6df1788ab514a81d269eed63
Defucilis <defucilis@gmail.com>
2021-03-17 19:04:44 +1100

    build

--- lib/Handy.d.ts
+++ lib/Handy.d.ts
@@ -1,4 +1,4 @@
-import { CsvUploadResponse, HandyMode, ModeResponse, SetSpeedResponse, SetStrokeResponse, SettingsResponse, StatusResponse, SyncOffsetResponse, SyncPlayResponse, SyncPrepareResponse, VersionResponse } from "./types";
+import { CommandResponse, CsvUploadResponse, HandyMode, ModeResponse, SetSpeedResponse, SetStrokeResponse, SettingsResponse, StatusResponse, SyncOffsetResponse, SyncPlayResponse, SyncPrepareResponse, VersionResponse } from "./types";
 declare class Handy {
     _connectionKey: string;
     serverTimeOffset: number;
@@ -9,6 +9,7 @@ declare class Handy {
     toggleMode(mode: HandyMode): Promise<ModeResponse>;
     setSpeed(speed: number, absolute?: boolean): Promise<SetSpeedResponse>;
     setStroke(speed: number, absolute?: boolean): Promise<SetStrokeResponse>;
+    setStrokeZone(min: number, max: number): Promise<CommandResponse>;
     stepSpeed(directionUp: boolean): Promise<SetSpeedResponse>;
     stepStroke(directionUp: boolean): Promise<SetStrokeResponse>;
     getVersion(): Promise<VersionResponse>;
@@ -18,6 +19,7 @@ declare class Handy {
     syncPrepare(scriptUrl: string, name?: string, size?: number): Promise<SyncPrepareResponse>;
     syncPlay(play?: boolean, time?: number): Promise<SyncPlayResponse>;
     syncOffset(offset: number): Promise<SyncOffsetResponse>;
+    syncAdjustTimestamp(videoTimeSeconds: number, filter?: number): Promise<boolean>;
     uploadCsv(csv: File, filename?: string): Promise<CsvUploadResponse>;
     enforceConnectionKey(): void;
     getUrl(cmd: string): string;
