ea99c7231ae8d6002cdf01989e677fe84f464bd6
Defucilis <defucilis@gmail.com>
2023-09-11 10:03:53 +1000

    feat: Add type declarations as named exports to index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,7 @@
 import Handy from "./Handy";
 import HandyLegacy from "./HandyLegacy";
+import { CsvUploadResponse, GenericResult, HampState, HandyFirmwareStatus, HandyInfo, HandyMode, HandySettings, HandyStatus, HsspSetupResult, HsspState, SetHampStateResult, SetHdspResult, SetModeResult, SlideInfo } from "./types";
 export { HandyLegacy };
+export { CsvUploadResponse, GenericResult, HampState, HandyFirmwareStatus, HandyInfo, HandyMode, HandySettings, HandyStatus, HsspSetupResult, HsspState, SetHampStateResult, SetHdspResult, SetModeResult, SlideInfo, };
 export default Handy;
 //# sourceMappingURL=index.d.ts.map
